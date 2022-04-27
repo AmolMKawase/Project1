97bebef27df1d1996c3d8a0782b3ea81c58c8712
amol k <amol k@AmolK>
2022-04-27 13:48:17 +0530

    by amol

---
+++ demo_git/src/demo_git/Simple_project.java
@@ -0,0 +1,33 @@
+package demo_git;
+
+public class Simple_project {
+
+	
+	public void m1()
+	{
+		System.out.println("m1");
+	}
+	
+	public void m2()
+	{
+		System.out.println("m2");
+	}
+	
+	public void m3()
+	{
+		System.out.println("m3");
+	}
+	
+	public void m4()
+	{
+		System.out.println("m4");
+	}
+	public static void main(String[] args) {
+		
+		Simple_project sp=new Simple_project();
+		sp.m1();
+		sp.m2();
+		sp.m3();
+		sp.m4();
+	}
+}
